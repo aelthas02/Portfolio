bef09c9b58a15959d6fd52ab4ee0c3e1c4642155
bmoreira <bmoreira@atech.com.br>
2021-07-22 15:18:56 -0300

    about done

--- src/app/components/main-page/contact/contact.component.ts
+++ src/app/components/main-page/contact/contact.component.ts
@@ -9,8 +9,10 @@ export class ContactComponent implements OnInit {
 
   public linkedinlogo: string = '../../../assets/LinkedIn-Logo.png';
   public githublogo: string = '../../../assets/GitHub-Logo.png';
+  public codepenlogo: string = '../../../assets/CodePen-Logo.png';
   public linkedinAddress: string = 'https://www.linkedin.com/in/breno-moreira-891504103/';
   public githubAddress: string = 'https://github.com/aelthas02/'; 
+  public codepenAddress: string = 'https://codepen.io/Wyndrin';
 
   constructor() { }
 
